a19f792e3168692debb97eadde69d64a5d4b8e05
sorinkus <thomcoucke@gmail.com>
2026-05-06 14:19:59 +0200

    Importation des MNT artificiels

---
+++ Données artificielles-20260506/Toma.py
@@ -0,0 +1,7 @@
+# Importation des différentes fonctions de calcul de pente et d'exposition.
+from pentes import *
+
+# Dimensions des terrains artificiels
+x = np.arange(0, 101)
+y = np.arange(0, 101)
+X, Y = np.meshgrid(x, y)
