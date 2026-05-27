169da5d87b41f225cde7875fac21e46c183b16b0
sorinkus <thomcoucke@gmail.com>
2026-05-27 16:11:42 +0200

    new file:   seance_1/Toma/Galerie d'images pour le rapport/exposition_dunkerque.png 	new file:   seance_1/Toma/Galerie d'images pour le rapport/pente_dunkerque.png 	new file:   seance_1/Toma/affichage_des_expos_Dunkerque.py 	modified:   seance_1/Toma/affichage_des_pentes_dunkerque.py 	deleted:    seance_1/Toma/pente.png

--- seance_1/Toma/affichage_des_expos_Dunkerque.py
+++ seance_1/Toma/affichage_des_expos_Dunkerque.py
@@ -14,12 +14,12 @@ data = np.loadtxt(dune_path)
 x = np.arange(0, 1249)
 y = np.arange(0, 1237)
 X, Y = np.meshgrid(x, y)
-Z = pente(*Evans(data))
+Z = exposition(*Evans(data))
 cmap = plt.cm.gist_earth
 img = plt.contourf(X, Y, Z, levels=100, cmap=cmap)
 # Autre possibilité
 # img = plt.pcolormesh(X, Y, data, cmap=cmap)
-plt.title('Dunkerque Pentes')
-plt.colorbar(img, label='pente')
+plt.title('Dunkerque Exposition')
+plt.colorbar(img, label='exposition')
 
 plt.show()
