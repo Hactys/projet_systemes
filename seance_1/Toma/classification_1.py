6ed1cbcd1150e87b967c701264376ad366609839
capdevillex <capdeville.x@gmail.com>
2026-06-01 15:05:43 +0200

    miaou version 2^pour le viccccc

--- seance_1/Toma/classification_1.py
+++ seance_1/Toma/classification_1.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from matplotlib.patches import Patch
 from recap import calcul_BPI
 from pentes_Toma import pente, Evans
+from rugosite2 import matrice_rugo
 
 
 class Terrain:
@@ -20,7 +21,7 @@ class Terrain:
 
 
 def classif_1(mnt):
-    bpi = calcul_BPI(mnt, r=50)
+    bpi = calcul_BPI(mnt, r=40)
     # FIX : initialiser à NAN plutôt qu'à 0 pour éviter des pixels PLAT parasites
     classe = np.full_like(mnt, Terrain.NAN, dtype=float)
     pts = pente(*Evans(mnt))
@@ -63,9 +64,17 @@ def classif_2(mnt):
 
 
 def afficher_classifications(mnt):
+    
+
     mnt = mnt[::-1, ::]
     classif1 = classif_1(mnt)
     # classif2 = classif_2(mnt)
+    print("calcul de rugo en cours")
+    mat_rugo = matrice_rugo()
+    print("calcul rugo fini")
+    mat_rugo = mat_rugo[::-1, ::]
+
+    rugo_haute = np.where(mat_rugo > 0.006, 1, np.nan)
 
     _, ax = plt.subplots(figsize=(12, 6))
 
@@ -88,17 +97,19 @@ def afficher_classifications(mnt):
         return cmap(norm(val))
 
     legend_elements = [
-        Patch(facecolor=c(Terrain.DEPRESSION),  edgecolor="k", label="Versant doux"),
-        Patch(facecolor=c(Terrain.PENTE),        edgecolor="k", label="Versant abrupte"),
+        Patch(facecolor=c(Terrain.DEPRESSION),  edgecolor="k", label="Versant abrupte"),
+        Patch(facecolor=c(Terrain.PENTE),        edgecolor="k", label="Versant doux"),
         Patch(facecolor=c(Terrain.CRETE),        edgecolor="k", label="Crête"),
         Patch(facecolor=c(Terrain.DEEPFLAT),     edgecolor="k", label="Deep Flat"),
         Patch(facecolor=c(Terrain.MIDFLAT),      edgecolor="k", label="Mid Flat"),
         Patch(facecolor=c(Terrain.SHALLOWFLAT),  edgecolor="k", label="Shallow Flat"),
         Patch(facecolor=c(Terrain.NAN),          edgecolor="k", label="NaN / bord"),
         Patch(facecolor="none", edgecolor="k", hatch="///", label="Ridules (hachures)"),
+        Patch(facecolor="red", edgecolor="k", label="Rugosité")
     ]
 
     ax.legend(handles=legend_elements, bbox_to_anchor=(1.05, 1), loc="upper left")
+    im_rugo = ax.imshow(rugo_haute, cmap="Reds", vmin=0, vmax=1, alpha=0.5, interpolation='none', zorder=10)
     plt.tight_layout()
     plt.show()
 
